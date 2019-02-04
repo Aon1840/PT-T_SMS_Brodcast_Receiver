694c6f6c5275ca1b4b3c91d732a9529886f039a9
PerumalTTiMac <imac@PerumalTTiMacs-iMac.local>
2019-02-04 18:04:44 +0700

    Part2

--- app/src/main/java/com/example/myfirstapplication/SmsBroadcastReceiver.java
+++ app/src/main/java/com/example/myfirstapplication/SmsBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
 import android.util.Log;
@@ -13,23 +14,26 @@ import okhttp3.internal.http2.Http2Connection;
 
 public class SmsBroadcastReceiver extends BroadcastReceiver {
 
-    private static final String TAG = "SmsBroadcastReceiver";
-
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)){
-            String smsSender = "";
-            String smsBody = "";
-            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)){
-                smsSender = smsMessage.getDisplayOriginatingAddress();
-                smsBody += smsMessage.getMessageBody();
-            }
-            Log.d(TAG, "SMS detected: From " + smsSender + " With text " + smsBody);
-            if (smsBody.startsWith("Hello")) {
-                Log.d(TAG, "Sms with (Hello) condition detected");
-                Toast.makeText(context, "BroadcastReceiver caught conditional SMS: " + smsBody, Toast.LENGTH_LONG).show();
-            }
+
+        if (!intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
+            return;
         }
+
+        Bundle bundle = intent.getExtras();
+        Object[] data = (Object[]) bundle.get("pdus");
+
+        String body = "";
+
+        for (int i=0; i< data.length; i++){
+            SmsMessage message = SmsMessage.createFromPdu((byte[]) data[i]);
+            body += message.getMessageBody();
+        }
+
+        String phone = SmsMessage.createFromPdu((byte[]) data[0]).getDisplayOriginatingAddress();
+        Toast.makeText(context,"SMS detected: From " + phone + " With text " + body,Toast.LENGTH_LONG).show();
+        Log.d("TESTTTTT----", "SMS detected: From " + phone + " With text " + body);
+
     }
 }
